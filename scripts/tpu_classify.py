e920d563c131100ca17c665aae9916a4fb3c9d44
comdet <listzone@hotmail.com>
2022-12-09 04:37:34 +0000

    update

--- scripts/tpu_classify.py
+++ scripts/tpu_classify.py
@@ -40,11 +40,13 @@ class image_feature:
         self.image_pub = rospy.Publisher("/output/image_detected/compressed", CompressedImage, queue_size = 5, tcp_nodelay=False)
         self.tpu_objects_pub = rospy.Publisher("/tpu_objects", tpu_objects, queue_size = 5, tcp_nodelay=False)
 
+        #Init Node
+        rospy.init_node('image_class', anonymous=False)
+
         # subscribed Topic
         self.subscriber = rospy.Subscriber("/output/image_raw/compressed", CompressedImage, self.callback,  queue_size = 5, tcp_nodelay=False)
         self.size = 320, 240
         
-        rospy.init_node('image_class', anonymous=False)
   
     def preprocess(self, img):
         
