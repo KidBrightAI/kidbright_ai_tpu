6590387004c359928d6a622480b0f42caa339e37
comdet <listzone@hotmail.com>
2023-02-09 17:07:30 +0700

    update camera

--- scripts/cam_stream.py
+++ scripts/cam_stream.py
@@ -5,11 +5,44 @@ import rospy
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CompressedImage
 
+
+def csi_pipeline(
+    sensor_id=0,
+    capture_width=640,
+    capture_height=480,
+    display_width=640,
+    display_height=480,
+    framerate=10,
+    flip_method=0
+):
+    return ('nvarguscamerasrc sensor-id=%d ! '
+                'video/x-raw(memory:NVMM), '
+                'width=(int)%d, height=(int)%d, '
+                'format=(string)NV12, framerate=(fraction)%d/1 ! '
+                'nvvidconv flip-method=%d ! '
+                'video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! '
+                'videoconvert ! '
+                'video/x-raw, format=(string)BGR ! appsink' % (sensor_id,
+                                                               capture_width, capture_height, framerate, flip_method,
+                                                               display_width, display_height))
+def platfrom():
+    with open("/proc/device-tree/model", "r") as f:
+        model = f.read().strip()
+        if "Jetson Nano" in model:
+            return "JETSON"
+        elif "Raspberry Pi" in model:
+            return "RPI"
+        else:
+            return "NANOPI"
+
 def camThread():
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH,320)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
-    cap.set(cv2.CAP_PROP_FPS, 10)
+    if platfrom() == "JETSON":
+        cap = cv2.VideoCapture(csi_pipeline(), cv2.CAP_GSTREAMER)
+    else:
+        cap = cv2.VideoCapture(0)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH,320)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
+        cap.set(cv2.CAP_PROP_FPS, 10)
     image_pub = rospy.Publisher("/output/image_raw/compressed", CompressedImage, queue_size=5, tcp_nodelay=False)
     rospy.init_node('cam_stream', anonymous=False)
     rate = rospy.Rate(10) # 10hz
