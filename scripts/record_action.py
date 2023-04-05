b9fb7d06d7362a8be0a8c9bcc491f372eb54eeb3
comdet <listzone@hotmail.com>
2023-04-05 21:41:12 +0700

    update record

--- scripts/record_action.py
+++ scripts/record_action.py
@@ -14,6 +14,7 @@ from kidbright_tpu.msg import recordResult
 from kidbright_tpu.msg import recordFeedback
 import kidbright_tpu.msg
 import os
+import io
 from datetime import datetime
 import time
 import base64
@@ -21,7 +22,7 @@ import base64
 SAMPLE_RATE = 44100
 THRESHOLD = 10 # in dB
 FRAME_PER_SEC = 20
-TIMEOUT_SEC = 15
+TIMEOUT_SEC = 10
 MFCC_NUM = 16
 
 from queue import Queue
@@ -39,115 +40,44 @@ class saveWave(object):
     self.record_started = False
     self.q = Queue()    
     self.frame_counter = 0
-    self.timeout_counter = 0
-    # Set parameters - MFCC
     self.snd_data = []
     #-------------------
-    self._action_server.start()
-    
+    self._action_server.start()    
     # 44100*4/2205
 
-#   def is_silent(self, snd_data, thres):   
-#     xx = np.frombuffer(base64.b64decode(snd_data), dtype=np.int16).astype(np.float32)
-#     volume_norm = np.linalg.norm(xx/65536.0)*10
-#     return volume_norm  < thres
-
-#   def callback(self, msg):
-#     self.timeoutCounter = self.timeoutCounter + 1
-#     if self.is_silent(msg.data, THRESHOLD) == False and self.frame_count == 0:
-#         self.START_REC = True
-#     if self.timeoutCounter % 20 == 0  and self.START_REC == False:
-#         print("PUBLISED")
-#         self._feedback.status = "StartRec"
-#         self._action_server.publish_feedback(self._feedback)
-
-#     if self.timeoutCounter == 100 and self.START_REC == False:
-#         self.timeoutCounter = 0
-#         print("TERMINATED")
-#         self._feedback.status = "Timeout"
-#         self._action_server.publish_feedback(self._feedback)
-#         self.a1_sub.unregister()
-#         self.q.put(2)
-
-#     if self.frame_count < self.nFrame and self.START_REC == True:    
-#         self.frame_count += 1
-#         self.obj.writeframesraw(base64.b64decode(msg.data))
-
-#         # Append msg from publisher to list
-#         da_o = np.frombuffer(base64.b64decode(msg.data), dtype=np.int16)
-#         print(da_o)
-            
-#         if(self.is_silent(msg.data, THRESHOLD)):
-#             print("SILENT")
-#         else:
-#             print("START REC")
-#         self.snd_data.extend(da_o)
-
-#             # Print log
-#         print("here")
-#         self._feedback.status = "Recording"
-#         self._action_server.publish_feedback(self._feedback)
-#         print(self.nFrame)
-#         print(self.frame_count)
-
-#         # Close wav object
-#         if self.frame_count == self.nFrame :
-#             self.obj.close()
-            
-#             print('Wav file saved successfully.')
-                  
-#     elif self.frame_count == self.nFrame: # once recording is done
-#         self._feedback.status = "MFCC"
-#         self._action_server.publish_feedback(self._feedback)
-#         print('Number of frames recorded: ' + str(len(self.snd_data)))
-#         mfccs = python_speech_features.base.mfcc(np.array(self.snd_data), 
-#                                         samplerate=self.sampleRate,
-#                                         winlen=0.256,
-#                                         winstep=0.050,
-#                                         numcep=MFCC_NUM,
-#                                         nfilt=26,
-#                                         nfft=2048,
-#                                         preemph=0.0,
-#                                         ceplifter=0,
-#                                         appendEnergy=False,
-#                                         winfunc=np.hanning)
-
-
-#         mfccs = mfccs.transpose()
-#         np.set_printoptions(suppress=True)
-   
-#         print('MFCC shape: ' + str(mfccs.shape))
-   
-#         np.savetxt(self.MFCCTextFileName, mfccs, fmt='%f', delimiter=",")
-
-#         # Create and save MFCC image
-#         plt.imshow(mfccs, cmap='inferno', origin='lower')
-#         plt.savefig(self.MFCCImageFileName)
-#         print('MFCC saved successfully.')
-
-#         # Shutdown node
-#         print("Unsubscribe down")
-#         self.a1_sub.unregister()
-#         self._feedback.status = "Done"
-#         self._action_server.publish_feedback(self._feedback)
-#         self.q.put(1)
-
+  def is_silent(self, snd_data, thres):
+    frames = np.frombuffer(snd_data, dtype=snd_data.typecode).astype(np.float32)
+    volume_norm = np.linalg.norm(frames/65536.0)*10
+    return volume_norm < thres
+  
   def callback(self, msg):
-    print(len(msg.data))
+    # message data len = 2205
+    self.frame_counter += 1
+    
+    if self.is_silent(msg.data, THRESHOLD) == False:
+      self.record_started = True
+      self._feedback.status = "START_RECORD"
+      self._action_server.publish_feedback(self._feedback)
 
+    if self.record_started and self.frame_counter % 20 == 0:
+      self._feedback.status = "RECORDING"
+      self._action_server.publish_feedback(self._feedback)
+    
+    if self.record_started:
+      self.snd_data.extend(msg.data)
+
+    if self.frame_counter >= self.nFrame:
+      self.audio_sub.unregister()
+      self.q.put(1)
+      
   def execute_cb(self, goal):
     self.frame_counter = 0
-    self.timeout_counter = 0
     self.record_started = False
     self.snd_data = []
-
-
 
     self.nFrame = goal.duration*FRAME_PER_SEC
     self.projectName = goal.projectname
 
-    print("Goal = ")
-    print(goal)
     print(f"n frame : {self.nFrame}")
 
     self.audio_sub = rospy.Subscriber("audio_int", kidbright_tpu.msg.int1d, self.callback, queue_size=4)
@@ -157,20 +87,53 @@ class saveWave(object):
     self._action_server.publish_feedback(self._feedback)
 
     #----- feedback result -------#    
-    # timeout = time.time() + TIMEOUT_SEC
-    # while self.q.empty():
-    #     if time.time() > timeout:
-    #         break
-    #     rospy.sleep(0.1)
-    # rr = self.q.get()
-    # if rr == 1:
-    #     self._result.result = "Done"
-    # else:
-    #     self._result.result = "TimeOut"
+    timeout = time.time() + TIMEOUT_SEC
+    while self.q.empty():
+      if time.time() > timeout:
+        break
+      rospy.sleep(0.1)
     
-    # rospy.loginfo('%s: Succeeded' % self._action_name)
-    # self._action_server.set_succeeded(self._result)
-    #------------------------------#  
+    record_result = self.q.get()
+    if record_result == 1:
+      # save wav
+      wavefile = wave.open(self.fileName,'w')
+      wavefile.setnchannels(1) # mono
+      wavefile.setsampwidth(2)
+      wavefile.setframerate(self.sampleRate)
+      wavefile.writeframesraw(self.snd_data)
+      wavefile.close()
+
+      # create mfcc
+      print('Number of frames recorded: ' + str(len(self.snd_data)))
+      mfccs = python_speech_features.base.mfcc(np.array(self.snd_data), 
+                                    samplerate=self.sampleRate,
+                                    winlen=0.256,
+                                    winstep=0.050,
+                                    numcep=MFCC_NUM,
+                                    nfilt=26,
+                                    nfft=2048,
+                                    preemph=0.0,
+                                    ceplifter=0,
+                                    appendEnergy=False,
+                                    winfunc=np.hanning)
+      mfccs = mfccs.transpose()
+      plt.imshow(mfccs, cmap='inferno', origin='lower')
+      #buf = io.BytesIO()
+      #plt.savefig(buf, format='png')
+      #buf.seek(0)
+      #plt.savefig(self.MFCCImageFileName)
+      
+      # with open("img.png", "rb") as image:
+      #   f = image.read()
+      #   b = bytearray(f)
+      #   print b[0]
+
+      #publish result
+      self._result.result = "SUCCESS"
+    else:
+      self._result.result = "TIMEOUT"
+    
+    self._action_server.set_succeeded(self._result)
 
 if __name__ == '__main__':
   saveWave()
